a1c116d052dc367b2326a41923c6dde74d9902a8
andy <andydavies2020@gmail.com>
2020-11-04 10:55:22 +0200

    serve: enable live reload

--- serve.sh
+++ serve.sh
@@ -5,5 +5,6 @@ docker run \
     -it \
     -v "$PWD:/srv/jekyll" \
     -p 4000:4000 \
+    -p 4040:4040 \
     jekyll/jekyll:pages \
-    jekyll serve --watch
+    jekyll serve --watch --livereload --livereload-port 4040
