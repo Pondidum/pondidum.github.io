ea612f4db0a1f324234bf47f075ba2dd0a4ba26e
andy <andydavies2020@gmail.com>
2021-11-06 13:20:08 +0200

    jekyll: update gems and local dev script

--- serve.sh
+++ serve.sh
@@ -1,10 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-docker run \
-    --rm \
-    -it \
-    -v "$PWD:/srv/jekyll" \
-    -p 4000:4000 \
-    -p 4040:4040 \
-    jekyll/jekyll:pages \
-    jekyll serve --watch --livereload --livereload-port 4040
+bundle exec jekyll serve --watch --livereload --livereload-port 4040
