8c1fd94ed9c26e39b61d90e72eea7e10b383023d
andy <andydavies2020@gmail.com>
2022-04-25 11:49:33 +0300

    jekyll: remove

--- serve.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-bundle exec jekyll serve --watch --livereload --livereload-port 4040
--- tags.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-ls _posts/*.md | xargs grep -h '^tags:.*' | sed 's/tags: //g' | sed 's/ /\n/g' | sort | uniq -c
